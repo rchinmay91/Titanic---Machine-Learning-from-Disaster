e3dcc0c3b1c285035bfe037dade3c5a4582ddd2a
rchinmay91 <rchinmay2002@gmail.com>
2026-05-13 23:38:25 +0530

    added titanic ML medel

---
+++ train.py
@@ -0,0 +1,62 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+# Load data
+train = pd.read_csv("data/train.csv")
+
+# Basic preprocessing
+train["Age"] = train["Age"].fillna(train["Age"].median())
+train["Embarked"] = train["Embarked"].fillna(train["Embarked"].mode()[0])
+
+# Encode gender
+train["Sex"] = train["Sex"].map({
+    "male": 0,
+    "female": 1
+})
+
+# Encode embarked
+train["Embarked"] = train["Embarked"].map({
+    "S": 0,
+    "C": 1,
+    "Q": 2
+})
+
+# Features
+features = [
+    "Pclass",
+    "Sex",
+    "Age",
+    "SibSp",
+    "Parch",
+    "Fare",
+    "Embarked"
+]
+
+X = train[features]
+y = train["Survived"]
+
+# Split data
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
+# Train model
+model = RandomForestClassifier(
+    n_estimators=200,
+    random_state=42
+)
+
+model.fit(X_train, y_train)
+
+# Predict
+pred = model.predict(X_test)
+
+# Accuracy
+acc = accuracy_score(y_test, pred)
+
+print("Accuracy:", acc)
